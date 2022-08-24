d98040318684201c6b72709faee04eaf410deb9d
mokdanbi <l26tempo@naver.com>
2022-08-24 12:21:32 +0900

    0824

--- react_basic00/src/App.js
+++ react_basic00/src/App.js
@@ -1,10 +1,20 @@
-import TopBanner from './pages/TopBanner';
+import List from './pages/ListTest';
 import './basic.css';
+import { useEffect, useState } from 'react';
+
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setData(json))
+  }, [])
+
   return (
     <>
-      <TopBanner />
+      {data[0].id}
     </>
   )
 }
